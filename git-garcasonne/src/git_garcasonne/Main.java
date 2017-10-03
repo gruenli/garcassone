4356e0b396975d6cd7d1d02c96db4e11f678d4d3
tommy <tommy@192.168.0.103>
2017-10-03 16:00:08 +0200

    Wieder mit allen Dateien

---
+++ git-garcasonne/src/git_garcasonne/Main.java
@@ -0,0 +1,18 @@
+package git_garcasonne;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application 
+{
+	@Override
+	public void start(Stage stage) throws Exception
+	{
+		Game game = new Game(stage);
+	}
+	
+	public static void main(String[] args)
+	{		
+		launch(args);
+	}
+}
